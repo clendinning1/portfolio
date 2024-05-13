d380ec0d14f18fcdfec16e372f2f6db519d364f0
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-05-12 19:28:15 -0500

    important comment w/ nav implementation in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     return (
         <>
             <Nav />
+            {/* nav will be in header later, just putting it here for now for testing */}
             <main className="mx-3">
                 {/* outlet component allows for routing */}
                 <Outlet />
