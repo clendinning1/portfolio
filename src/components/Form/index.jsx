437e78e45d19322412c55b21d52988a7d40302c2
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-05-14 20:04:16 -0500

    added onsubmit funct

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -7,32 +7,44 @@ function Form() {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
 
+    const onSubmit = (e) => {
+        // prevent page refresh
+        e.preventDefault();
+
+        // notify user on succesful submit
+        alert(`Thanks ${name}, your message was sent.`);
+        // clear inputs
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
+
     return (
         <div className="container text-center">
             <h1>
                 Hello {name} {email} {message}
             </h1>
-            <form className="form" onSubmit={}>
+            <form className="form" onSubmit={onSubmit}>
                 <input
                     value={name}
                     name="name"
                     onChange={}
                     type="text"
-                    placeholder="Name"
+                    placeholder="name"
                 />
                 <input
                     value={email}
                     name="email"
                     onChange={}
                     type="text"
-                    placeholder="Email"
+                    placeholder="email"
                 />
                 <input
                     value={message}
-                    name=""
+                    name="message"
                     onChange={}
                     type="text"
-                    placeholder="Message"
+                    placeholder="message"
                 />
                 <button type="submit">
                     Submit
